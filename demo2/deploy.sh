2ca69358b58410eb7e3edb7c0a624c0784b8de1e
Luke Thompson <endeavour9@gmail.com>
2018-08-26 11:36:52 +1000

    simplify demo #2 - it's just about dns

--- demo2/deploy.sh
+++ demo2/deploy.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-SUBDOMAIN=${1:-cfn-cf-demo1}
-CLOUDFRONT_DOMAIN=$2
+# Percentage transitioned from legacy is the 1st parameter - a value between 0 and 100
 declare -i CLOUDFRONT_WEIGHT
-CLOUDFRONT_WEIGHT=$3
+CLOUDFRONT_WEIGHT=${1:-0}
 LEGACY_WEIGHT=$(expr 100 - $CLOUDFRONT_WEIGHT)
 
+SUBDOMAIN=${2:-cfn-cf-demo2}
+CLOUDFRONT_DOMAIN=${3:-awesome-and-thrifty.cloudfront.com}
+LEGACY_DOMAIN=${4:-awesome-but-pricey.akamai.com}
+
+echo "Parameters passed:
+./deploy.sh "$CLOUDFRONT_WEIGHT" "$SUBDOMAIN" "$CLOUDFRONT_DOMAIN" "$LEGACY_DOMAIN"
+"
+
 echo "Started CloudFormation deployment of "$SUBDOMAIN".mycodefu.com, "$CLOUDFRONT_WEIGHT"% migrated ("$LEGACY_WEIGHT"% legacy) at "`date`"..."
-aws cloudformation deploy --region us-east-1 --template-file route53-transition.yml --stack-name $SUBDOMAIN-weighted-dns \
+aws cloudformation deploy --region us-east-1 --template-file template.yml --stack-name $SUBDOMAIN-weighted-dns \
         --parameter-overrides \
             "HostedZoneName=mycodefu.com." \
             "WebsiteAddress="$SUBDOMAIN".mycodefu.com" \
-            "LegacyCDNDomain=ec2-52-64-14-196.ap-southeast-2.compute.amazonaws.com" \
+            "LegacyCDNDomain="$LEGACY_DOMAIN \
             "LegacyCDNDomainWeight="$LEGACY_WEIGHT \
             "CDNDomain="$CLOUDFRONT_DOMAIN \
             "CDNDomainWeight="$CLOUDFRONT_WEIGHT
@@ -20,4 +27,8 @@ aws cloudformation describe-stacks --stack-name $SUBDOMAIN-weighted-dns --query 
 
 echo "Finished CloudFormation deploy at "`date`
 
-../demo1/test.sh "http://"$SUBDOMAIN".mycodefu.com"
+echo "Check deployment propogation at:
+https://dnschecker.org/#CNAME/"$SUBDOMAIN".mycodefu.com
+or
+https://www.whatsmydns.net/#CNAME/"$SUBDOMAIN".mycodefu.com
+"
